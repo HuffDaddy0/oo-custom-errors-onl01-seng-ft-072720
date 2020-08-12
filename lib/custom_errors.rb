3d1fd03e7dc17df41fcd0ca20c73d677236cb04b
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 19:32:09 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -15,17 +15,12 @@ else
   end
 
   class PartnerError < StandardError
-    
-    
-    
+
+
+
   end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
-
